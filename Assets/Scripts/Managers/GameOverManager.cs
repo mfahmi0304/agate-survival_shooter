eb4131ba43e99c6d23f8319d0d2e3d5266f2a38f
unknown <m.fahminuraziz93@gmail.com>
2021-09-29 20:25:02 +0700

    [UPDATE]

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;
+    
+    public bool isDead = false;
+
+    Animator anim;
+    
+    float restartTimer = 0.0f;
+    float restartDelay = 5.0f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+ 
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            if(!isDead){
+                Debug.Log("Game Over");
+                anim.SetTrigger("GameOver");
+                isDead = true;
+            }
+
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer >= restartDelay){
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+ 
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m",Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int scoreValue;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        scoreValue = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + scoreValue;
+    }
+}
